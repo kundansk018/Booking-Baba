2faeb08a7d48efed08635b325045bbca65483d0d
Kundan Savale <79473725+kundansk018@users.noreply.github.com>
2023-05-30 20:22:02 +0530

    added get all order history api

--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -129,25 +129,23 @@ export default function Hotels() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              {hotelData
-                ? hotelData.data.map((element: any) => (
-                    <>
-                      <td className="w-[5px]">{"Photo"}</td>
-                      <td className="w-[5px]">{element.hotelname}</td>
-                      <td className="w-[5px]">{element.ownerName}</td>
-                      <td className="w-[5px]">{element.contactno}</td>
-                      <td className="w-[5px]">{element.email}</td>
-                      <td className="w-[5px]">{element.adress}</td>
-                      <td className="w-[5px]">{element.street}</td>
-                      <td className="w-[5px]">{element.city}</td>
-                      <td className="w-[5px]">{element.pin}</td>
-                      <td className="w-[5px]">{"contries"}</td>
-                      <td className="w-[5px]">{"Action btn"}</td>
-                    </>
-                  ))
-                : "Data Not Found.."}
-            </tr>
+            {hotelData
+              ? hotelData.data.map((element: any) => (
+                  <tr>
+                    <td className="w-[5px]">{"Photo"}</td>
+                    <td className="w-[5px]">{element.hotelname}</td>
+                    <td className="w-[5px]">{element.ownerName}</td>
+                    <td className="w-[5px]">{element.contactno}</td>
+                    <td className="w-[5px]">{element.email}</td>
+                    <td className="w-[5px]">{element.adress}</td>
+                    <td className="w-[5px]">{element.street}</td>
+                    <td className="w-[5px]">{element.city}</td>
+                    <td className="w-[5px]">{element.pin}</td>
+                    <td className="w-[5px]">{"contries"}</td>
+                    <td className="w-[5px]">{"Action btn"}</td>
+                  </tr>
+                ))
+              : "Data Not Found.."}
           </tbody>
         </table>
       </CardBody>
